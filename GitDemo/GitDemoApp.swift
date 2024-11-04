36700ffc418d912d4d9a72cf4c5f211e28e7c0fc
Mukhtaram <mukhtaramsulaimonov@gmail.com>
2024-11-04 10:38:09 +0100

    Initial Commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Mukhtaram Sulaimonov on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
